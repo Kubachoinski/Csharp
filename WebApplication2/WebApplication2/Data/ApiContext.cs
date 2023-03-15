dedfc300366d4591da2b6486cfc4790a43c072f2
Kubachoinski <113339346+Kubachoinski@users.noreply.github.com>
2023-03-15 13:01:34 +0100

    Add files via upload
    14.03

---
+++ WebApplication2/WebApplication2/Data/ApiContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Controllers;
+using WebApplication2.Models;
+
+
+namespace WebApplication2.Data
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<HotelBooking> Bookings { get; set; }
+
+        public ApiContext(DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
